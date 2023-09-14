707489de7a743be185d8e2e782b169d885438839
IrouKaizen <122926735+IrouKaizen@users.noreply.github.com>
2023-09-14 08:23:19 +0000

    Update 005_exercice_con_F_C.java

--- 005_exercice_con_F_C.java
+++ 005_exercice_con_F_C.java
@@ -57,3 +57,67 @@ public class Conversion {
         }
     }
     
+//Méthode 2
+
+//Programme de conversion pour passer des degrés Celsius en degrés Fahrenheit
+
+package javaapplication13;
+import java.util.Scanner;
+
+public class JavaApplication13 {
+    
+    public static void main(String[] args) {
+    //Notre objet Scanner
+    Scanner sc = new Scanner(System.in);
+    //initialisation des variables
+    double aConvertir, convertit=0;
+    char reponse=' ', mode = ' ';
+    //int conv = 2;
+    System.out.println("CONVERTISSEUR DEGRÉS CELSIUS ET DEGRÉS FAHRENHEIT \n");
+    do{//tant que reponse = O
+        
+        do{//tant que reponse n'est pas O ou N
+            mode = ' ';
+            
+            System.out.println("1 - Convertisseur Celsius - Fahrenheit");
+            System.out.println("2 - Convertisseur Fahrenheit - Celsius");
+            System.out.print("Choisissez le mode de conversion : ");
+            
+            mode = sc.nextLine().charAt(0);
+            if(mode != '1' && mode != '2')
+                System.out.println("Mode inconnu, veuillez réitérer votre choix.");
+            }while (mode != '1' && mode != '2');
+        
+                //saisie de la température à convertir
+                System.out.print("Température à convertir : ");
+                aConvertir = sc.nextDouble();
+                //Pensez à vider la ligne lue
+                sc.nextLine();
+                //Selon le mode, on calcule différemment et on affiche le résultat
+            
+            switch(mode){
+                case '1': 
+                    convertit = ((9.0/5.0) * aConvertir) + 32.0;
+                    //double value =  (double) ( (int) (convertit * Math.pow(10, 2) + .5)) / Math.pow(10, 2);
+                    System.out.print(aConvertir + " °C correspond à : " + arrondi(convertit, 2) + " °F.");//+"la valeur est"+ value);
+                break;
+                
+                case '2':
+                    convertit = ((aConvertir - 32) * 5) / 9;
+                    System.out.println(aConvertir + " °F correspond à : " + arrondi(convertit, 2) + " °C.");
+            }
+            //On invite l'utilisateur à recommencer ou à quitter
+            do{
+                System.out.print("Souhaitez-vous convertir une autre température ?(O/N) : ");
+                reponse = sc.nextLine().charAt(0);
+            }while(reponse != 'O' && reponse != 'N');
+            
+        }while(reponse == 'O');
+    
+            System.out.println("Au revoir !");
+            //Fin de programme
+        }
+        public static double arrondi(double A, int B) {
+        return (double) ( (int) (A * Math.pow(10, B) + .5)) / Math.pow(10, B);
+    }
+}
